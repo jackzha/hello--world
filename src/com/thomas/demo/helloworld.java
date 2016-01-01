cf8f325fa2481b29583610295c32b6916ae9b49a
Thomas_zha <zhaquanyi521@126.com>
2016-01-01 21:39:08 +0800

    how to use idea

---
+++ src/com/thomas/demo/helloworld.java
@@ -0,0 +1,10 @@
+package com.thomas.demo;
+
+/**
+ * Created by Administrator on 2016/1/1 0001.
+ */
+public class helloworld {
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
